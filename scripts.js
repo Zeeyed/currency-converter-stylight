d7368c7bb268d23dd78e765d698cb4b4078c28d1
Zied Ghalleb <zied.ghalleb@gmail.com>
2017-10-28 12:45:08 +0200

    Basic crrency exchange implementation

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 // Your JavaScript goes here.
+const DEFAULT_URL = axios.defaults.baseURL = 'http://api.fixer.io/latest';
+
+const getExchangeRate = (from, to) => {
+  return axios.get(`${DEFAULT_URL}?base=${from}`)
+    .then((rate) => {
+      return rate.data.rates[to];
+    })
+    .catch((error) => {
+      return error;
+    })
+}
+
+const convertCurrency = (from, to, amount) => {
+  return getExchangeRate(from, to)
+    .then((rate) => {
+      const convertedAmount = (amount * rate).toFixed(2);
+      return console.log(convertedAmount);
+    })
+}
