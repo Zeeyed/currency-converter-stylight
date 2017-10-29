597aee6b4b3851e952d1498f32df5cf9b4662386
Zied Ghalleb <zied.ghalleb@gmail.com>
2017-10-29 14:55:36 +0100

    Fix some minor bugs

--- scripts.js
+++ scripts.js
@@ -1,8 +1,43 @@
 // Your JavaScript goes here.
-const DEFAULT_URL = axios.defaults.baseURL = 'http://api.fixer.io/latest';
+// function
+// Random selection
+const randomItemFromArray = (array) => array[Math.floor(Math.random() * array.length)];
+const itemFromArrayExceptCurrent = (array, current) => {
+  return randomItemFromArray(array.filter((element) => {
+    return element !== current;
+  }));
+} 
+// SET DEFAULT VALUES
+const currencies = ['EUR', 'USD', 'JPY'];
+const URL_DEFAULT = axios.defaults.baseURL = 'http://api.fixer.io/latest';
+const AMOUNT_FROM_DEFAULT = document.getElementById('amount_from').defaultValue = '';
+const AMOUNT_TO_DEFAULT = document.getElementById('amount_to').defaultValue = '';
+const CURRENCY_FROM_DEFAULT = document.getElementById('amount_to').defaultValue = '';
+
+const selectFrom = document.getElementById('select-from');
+const selectTo = document.getElementById('select-to');
+
+const optionsFrom = document.createDocumentFragment();
+const optionsTo = document.createDocumentFragment();
+
+const getAllCurrencies = (curr) => {
+  curr.forEach((currency, index) => {
+    const option_currency_from = document.createElement('option');
+    const option_currency_to = document.createElement('option');
+    option_currency_from.innerHTML = currency;
+    option_currency_from.value = currency;
+    option_currency_to.innerHTML = currency;
+    option_currency_to.value = currency;
+    optionsFrom.appendChild(option_currency_from);
+    optionsTo.appendChild(option_currency_to);
+  });
+  selectFrom.appendChild(optionsFrom);
+  selectTo.appendChild(optionsTo);
+}
+getAllCurrencies(currencies)
 
 const getExchangeRate = (from, to) => {
-  return axios.get(`${DEFAULT_URL}?base=${from}`)
+  return axios.get(`${URL_DEFAULT}?base=${from}`)
     .then((rate) => {
       return rate.data.rates[to];
     })
@@ -13,14 +48,43 @@ const getExchangeRate = (from, to) => {
 
 const convertCurrency = (from, to, amount) => {
   return getExchangeRate(from, to)
-    .then((rate) => {
-      const convertedAmount = (amount * rate).toFixed(2);
-      return console.log(convertedAmount);
-    })
+  .then((rate) => {
+    const convertedAmount = (amount * rate).toFixed(2);
+    return convertedAmount;
+  }).catch((error) => {
+    console.log(error)
+  })
+}
+
+// get select value
+const currencyFromOnChange = () => {
+  const selectFromValue = document.getElementById('select-from').value;
+  const from = document.querySelector('input[name="amount_from"]').value;
+  const currenyTo = currencyToOnChange();
+  convertCurrency(selectFromValue, currenyTo, from).then((res) => {
+    return document.getElementById('amount_to').value = res
+  });
+  return  selectFromValue ? selectFromValue : 'EUR';
+}
+const currencyToOnChange = () => {
+  const selectToValue = document.getElementById('select-to').value;
+  return selectToValue ? selectToValue : 'EUR';
 }
 
 const onChange = () => {
-  const from = document.querySelector('input[name="currency-to"]').value;
+  const from = document.querySelector('input[name="amount_from"]').value;
   const to = document.querySelector('input[name="currency-to"]').value;
-  console.log('hhhhh', from)
+  const currencyFrom = currencyFromOnChange();
+  const currencyTo = currencyToOnChange();
+  if(currencyFrom === currencyTo || isNaN(to)){
+    document.getElementById('select-to').value = itemFromArrayExceptCurrent(currencies, currencyFrom);
+  }
+  convertCurrency(currencyFrom, currencyTo, from).then((res) => {
+    const currencyTo = document.getElementById('amount_to').value;
+    console.log(currencyTo)
+    if(isNaN(currencyTo)){
+      return document.getElementById('amount_to').value = '';
+    };
+    return document.getElementById('amount_to').value = res;
+  });
 }
